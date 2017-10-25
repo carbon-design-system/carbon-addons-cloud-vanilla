2c7a8160e1c63ae24b973f51db2a1debc4f4dc3f
Josh Black <joshblack@us.ibm.com>
2017-10-25 17:43:32 -0500

    docs: add webpack example

---
+++ examples/webpack/README.md
@@ -0,0 +1,11 @@
+# `example-webpack`
+
+> Development-based webpack config to show how to incorporate
+> `carbon-addons-bluemix` into a project.
+
+## Up & Running
+
+- Run `yarn install` to install all the necessary dependencies
+- Run `yarn dev` to start the `webpack-dev-server`
+- Visit `http://localhost:3000` to view an example card that is being rendered
+  with the SCSS provided from `carbon-addons-bluemix`
---
+++ examples/webpack/webpack.config.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const eyeglass = require('eyeglass');
+
+module.exports = {
+  devtool: 'cheap-module-soruce-map',
+  entry: [
+    path.resolve(__dirname, './src/index.js'),
+  ],
+  output: {
+    path: path.resolve(__dirname, './build'),
+    pathinfo: true,
+    filename: 'static/js/bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 2,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              sourceMap: true,
+              plugins: () => [
+                require('autoprefixer')({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9', // React doesn't support IE8 anyway
+                  ],
+                  flexbox: 'no-2009',
+                }),
+              ],
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              sourceMapContents: true,
+              includePaths: [
+                path.resolve(__dirname, 'node_modules'),
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+  ],
+  devServer: {
+    noInfo: true,
+    contentBase: path.resolve(__dirname, '../build'),
+    port: 3000,
+  },
+};
